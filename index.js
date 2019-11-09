978baca557b988f2b63f2b8548b1bec7b9e6726f
Dung <dung8421@gmail.com>
2019-11-09 20:04:17 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import app2 from './app2';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => app2);
+AppRegistry.registerComponent(appName, () => App);
